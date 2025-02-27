d1e00da390b270fe652b62227b1e9acbdd75ef2e
Yersain Castaño <yercas93@gmail.com>
2025-02-27 18:05:46 -0500

    📦️ chirps.blade.php was created in the path resourse/views.

---
+++ resources/views/chirps.blade.php
@@ -0,0 +1,10 @@
+<x-app-layout>
+    <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+        <livewire:chirps.create />
+    </div>
+</x-app-layout><x-app-layout>
+    <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+        <livewire:chirps.create />
+        <livewire:chirps.index />
+    </div>
+</x-app-layout>
